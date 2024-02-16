4858a1a2ae0f468628c0053f134d8b2d6c5d4e69
aestt <105264205+aestt@users.noreply.github.com>
2024-02-16 17:20:26 -0300

    Update ready.py

--- events/ready.py
+++ events/ready.py
@@ -7,17 +7,17 @@ class Ready(commands.Cog):
     def __init__(self, bot, *args, **kwargs):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_ready(self):
+    @commands.Cog.listener() # Cria um evento
+    async def on_ready(self): # Indica que o evento é on_ready, ou seja, quando o bot estiver pronto (online) irá acontecer alguma coisa
         print("-------------------------------------------")
-        print(f"Estou logado como: {self.bot.user.name}")
+        print(f"Estou logado como: {self.bot.user.name}") # Imprime o nome do bot + as mensagens no console
         print("-------------------------------------------")
 
-        with open("./config/app.json") as file:
+        with open("./config/app.json") as file: # Abre o arquivo JSON
             data = json.load(file)
 
-        atividade = data['STATUS_DE_ATIVIDADE']
-        await self.bot.change_presence(activity=nextcord.Game(name=atividade))
+        atividade = data['STATUS_DE_ATIVIDADE'] # Pega a váriavel de status de atividade
+        await self.bot.change_presence(activity=nextcord.Game(name=atividade)) # Seta o status de atividade de acordo com a variável
 
 def setup(bot):
     bot.add_cog(Ready(bot))
